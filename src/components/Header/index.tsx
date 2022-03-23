effad452d7ecf75d0abc525b89ea7bd3514115b9
Anna <sudolann@gmail.com>
2022-03-23 11:35:41 +0000

    header edited

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 
 export const Header = () => (
   <header className='h-22 w-full'>
-      <h1 className="text-gray-200 text-2xl uppercase text-center">Send Sol</h1>
+    <h1 className="text-gray-200 text-2xl uppercase text-center">Send Sol</h1>
   </header>
 );
