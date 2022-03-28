ec5351ba44874a598d624ad9def011ff5afe3fbf
Anna <sudolann@gmail.com>
2022-03-28 14:39:22 +0100

    select

--- src/components/SelectInput/index.tsx
+++ src/components/SelectInput/index.tsx
@@ -1,3 +1,5 @@
+import { CallbackFunctionText } from "@interfaces";
+
 type IoptionsType = {
     value: string,
     label: string
@@ -5,28 +7,14 @@ type IoptionsType = {
 interface ISelectInputnProps {
     options: IoptionsType[];
     selectedValue: string;
-    onChangeSelect: void;
+    onChangeSelect: CallbackFunctionText
 };
 
 export const SelectInput: React.FunctionComponent<ISelectInputnProps> = ({ options, selectedValue, onChangeSelect}) => {
     return(
-    <select className="form-select appearance-none
-        block
-        w-full
-        px-3
-        py-1.5
-        text-base
-        font-normal
-        text-gray-700
-        bg-white bg-clip-padding bg-no-repeat
-        border border-solid border-gray-300
-        rounded
-        transition
-        ease-in-out
-        m-0
-        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
+        <select className="border h-12 bg-white border-main-yellow text-gray-900 placeholder-gray-400 outline-none px-5 w-full text-sm rounded-lg focus:bg-white focus:border-main-yellow focus:outline-none" aria-label="Default select example">
             {options.map((option, i)=> <option selected={selectedValue === option.value} key={i} value={option.value} onChange={()=> onChangeSelect(option.value)}>{option.label}</option>)}
         </select>
-        )
-    }       
+    )
+}       
 
