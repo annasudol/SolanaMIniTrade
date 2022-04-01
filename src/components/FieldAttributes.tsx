4b35213dbd5ad0363686b8f0e26d284a2a9cfb4c
Anna <anjasudol@gmail.com>
2022-04-01 11:55:27 +0100

    added form

---
+++ src/components/FieldAttributes.tsx
@@ -0,0 +1,57 @@
+import clsx from "clsx";
+import uniqueId from "lodash/uniqueId";
+import { cloneElement, isValidElement, useEffect, useState } from "react";
+
+export interface FieldAttributesProps {
+  label?: string;
+  error?: string | false | string[];
+  className?: string;
+  defaultClassName?: string;
+  onLabelClick?: () => void;
+  disabled?: boolean
+}
+
+export const FieldAttributes: React.FC<FieldAttributesProps> = ({
+  label,
+  error,
+  className,
+  defaultClassName = "w-full mb-5",
+  children,
+  onLabelClick,
+  disabled,
+}) => {
+  const [id, setId] = useState<string | undefined>(undefined);
+  const classes = clsx(
+    "mb-1 text-sm font-medium text-gray-600 select-none pl-2",
+    {
+      "opacity-75": disabled,
+    },
+  );
+
+  useEffect(() => {
+    setId(uniqueId("id-"));
+  }, []);
+
+  return (
+    <div className={clsx("flex flex-col relative mb-12", defaultClassName, className)}>
+      {label && (
+        <label
+          aria-hidden="true"
+          className={classes}
+          htmlFor={id}
+          onClick={() => onLabelClick?.()}
+        >
+          {label}
+        </label>
+      )}
+
+      {label ? isValidElement(children) && cloneElement(children, { id }) : children}
+
+      {error && (
+        <small className="tg-caption-sm text-red-400 ml-2 mt-0.5 absolute top-full">
+          {Array.isArray(error) ? error[0] : error}
+        </small>
+      )}
+    </div>
+  );
+};
---
+++ src/components/TransactionForm.tsx
@@ -0,0 +1,43 @@
+import { Button, Input } from "@components";
+import { useForm } from "./Validation.hooks";
+import { WalletContext } from "@context";
+import React from "react";
+
+export const TransactionForm: React.FC = () => {
+  const { formik, errors } = useForm();
+  const [loading, setLoading] = React.useState(false);
+  const handleSend=()=> {
+    setLoading(true);
+  }
+
+        return(
+          <div className="sm:flex justify-center pt-12">
+            <form className="w-full sm:w-1/2" onSubmit={formik.handleSubmit}>
+              <Input
+                type="number"
+                step={0.1}
+                disabled={!walletKey}
+                {...formik.getFieldProps("amount")}  
+                fieldAttributesProps={{ error: walletKey && errors.amount.error, label: "Amount" }}
+                placeholder="Min 0.02"
+              >
+                  <div className="absolute top-9 right-4">
+                   {balance !== undefined && <button onClick={handleClick} className="text-main-lightBrown mr-2" type="submit">max</button>}
+                    <span className="uppercase text-gray-7 border-l border-gray-7 px-1">SOL</span>
+                  </div>
+              </Input>
+              <Input
+                {...formik.getFieldProps("address")}
+                fieldAttributesProps={{ error: walletKey && errors.address.error, label: "Solana Wallet Address" }}
+                placeholder="Enter address here"
+                disabled={!walletKey}
+              />
+              <Button onClick={handleSend} disabled={buttonIsdisabled} loading={loading}>Send</Button>
+            </form>
+          </div>
+        )
+};
+function props(props: any): [any] {
+  throw new Error("Function not implemented.");
+}
+
